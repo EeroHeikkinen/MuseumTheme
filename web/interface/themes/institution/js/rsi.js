0d478cbcfb402f5bbfae1ab6e732a3a548761881
Ere Maijala <ere.maijala@helsinki.fi>
2012-06-08 09:52:52 +0300

    Changed RSI error messages from dialog to a simple string added to the results.

--- web/interface/themes/institution/js/rsi.js
+++ web/interface/themes/institution/js/rsi.js
@@ -8,51 +8,53 @@ function checkRSI(source, peerReviewedOnly, texts) {
 
         url = path + '/AJAX/JSON_RSI?method=getRSIStatuses&source=' + source;
     	$.getJSON(url, {id:id}, function(response) {
-    		    if (response.status == 'OK') {
-    		        $('.rsi').removeClass('ajax_fulltext_availability');
-                    $.each(response.data, function(i, result) {
-                        var safeId = jqEscape(result.id);
-                        
-                        if (peerReviewedOnly) {
-                        	if (result.status == 'peerReviewedFullText') {
-    	                        var span = $('<span/>');
-    	                        span.addClass('rsi');
-    	                        span.addClass('peerReviewed');
-    	                        span.html(texts['peerReviewed']);
-    	                        $('#record' + safeId).find('.rsi').append(span);
-                        	}
-                        	return;
-                        }
-                        
-                        if (result.status == 'fullText' || result.status == 'peerReviewedFullText') {
-	                        var a = $('<a/>');
-	                        a.attr('href', $('#record' + safeId).find('.openUrl').parent().attr('href') + '&__service_type=getFullTxt');
-	                        a.addClass('rsi');
-	                        if (result.status == 'peerReviewedFullText') {
-	                        	a.addClass('peerReviewed');
-	                        }
-	                        a.html(texts[result.status]);
-	                        $('#record' + safeId).find('.rsi').append(a);
-                        } else if (result.status == 'noInformation') {
-		                    var a = $('#record' + safeId).find('.openUrl').parent();
-		                    var children = a.children();
-		                    a.html(texts['maybeFullText']).append(children);
-                        } else {
-	                        var span = $('<span/>');
-	                        span.addClass('rsi');
-	                        span.html(texts[result.status]);
-	                        $('#record' + safeId).find('.rsi').append(span);
-                        }
-                        if (result.status != 'noInformation') {
-		                    var a = $('#record' + safeId).find('.openUrl').parent();
-	                        var children = a.children();
-		                    a.html(texts['moreInformation']).append(children);
-                        }
-                    });
+	        $('.rsi').removeClass('ajax_fulltext_availability');
+		    if (response.status != 'OK') {
+		        $('.rsi').text("RSI status check failed.");
+		        return;
+		    }
+            $.each(response.data, function(i, result) {
+                var safeId = jqEscape(result.id);
+                
+                if (peerReviewedOnly) {
+                	if (result.status == 'peerReviewedFullText') {
+                        var span = $('<span/>');
+                        span.addClass('rsi');
+                        span.addClass('peerReviewed');
+                        span.html(texts['peerReviewed']);
+                        $('#record' + safeId).find('.rsi').append(span);
+                	}
+                	return;
                 }
+                
+                if (result.status == 'fullText' || result.status == 'peerReviewedFullText') {
+                    var a = $('<a/>');
+                    a.attr('href', $('#record' + safeId).find('.openUrl').parent().attr('href') + '&__service_type=getFullTxt');
+                    a.addClass('rsi');
+                    if (result.status == 'peerReviewedFullText') {
+                    	a.addClass('peerReviewed');
+                    }
+                    a.html(texts[result.status]);
+                    $('#record' + safeId).find('.rsi').append(a);
+                } else if (result.status == 'noInformation') {
+                    var a = $('#record' + safeId).find('.openUrl').parent();
+                    var children = a.children();
+                    a.html(texts['maybeFullText']).append(children);
+                } else {
+                    var span = $('<span/>');
+                    span.addClass('rsi');
+                    span.html(texts[result.status]);
+                    $('#record' + safeId).find('.rsi').append(span);
+                }
+                if (result.status != 'noInformation') {
+                    var a = $('#record' + safeId).find('.openUrl').parent();
+                    var children = a.children();
+                    a.html(texts['moreInformation']).append(children);
+                }
+            });
         }).error(function() {
 	        $('.rsi').removeClass('ajax_fulltext_availability');
-            alert("RSI query for full text encountered an error.");
+	        $('.rsi').text("RSI status check failed.");
         });              
     }
 }
