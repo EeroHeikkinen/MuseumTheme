99461909efbd45f435d3aeb58788ba511e74d54c
Ere Maijala <ere.maijala@helsinki.fi>
2012-11-01 07:51:50 +0200

    Display throbber to show that something is happening during Persona login verification.

--- web/interface/themes/institution/js/persona.js
+++ web/interface/themes/institution/js/persona.js
@@ -3,27 +3,32 @@ function mozillaPersonaSetup(currentUser)
     navigator.id.watch({
       loggedInUser: currentUser,
       onlogin: function(assertion) {
+        $("#personaLogin").addClass("persona-login-loading");
         $.ajax({
-          type: 'POST',
-          dataType: 'json',
+          type: "POST",
+          dataType: "json",
           url: path + "/AJAX/JSON_PersonaLogin?method=login",
           data: {
             assertion: assertion
           },
           success: function(response, status, xhr) { 
+            $("#personaLogin").removeClass("persona-login-loading");
             if (response.status == "OK") { 
               window.location.reload();
             } else {
               alert("Login failed");
             }
           },
-          error: function(xhr, status, err) { alert("login failure: " + err); }
+          error: function(xhr, status, err) { 
+              $("#personaLogin").removeClass("persona-login-loading");
+              alert("login failure: " + err); 
+          }
         });
       },
       onlogout: function() {
         $.ajax({
-          type: 'GET',
-          dataType: 'json',
+          type: "GET",
+          dataType: "json",
           url: path + "/AJAX/JSON_PersonaLogin?method=logout",
           success: function(response, status, xhr) { if (window.location.href != path) window.location = path; else window.location.reload() },
           error: function(xhr, status, err) { alert("logout failure: " + err); }
