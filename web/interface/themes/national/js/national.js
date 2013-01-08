0a9fb1a9a7f53427cbc93c2c65e26680fec1bf57
Erik Henriksson <erik.henriksson@helsinki.fi>
2013-01-08 13:34:44 +0200

    National theme specific javascript.

--- web/interface/themes/national/js/national.js
+++ web/interface/themes/national/js/national.js
@@ -1,45 +1,14 @@
 /**
- * Initialize functions used by the National layout
+ * Initialize National theme specific functions
  */
-/**
-* hoverIntent r6 // 2011.02.26 // jQuery 1.5.1+
-* <http://cherne.net/brian/resources/jquery.hoverIntent.html>
-* 
-* @param  f  onMouseOver function || An object with configuration options
-* @param  g  onMouseOut function  || Nothing (use configuration options object)
-* @author    Brian Cherne brian(at)cherne(dot)net
-*/
-(function($){$.fn.hoverIntent=function(f,g){var cfg={sensitivity:7,interval:100,timeout:0};cfg=$.extend(cfg,g?{over:f,out:g}:f);var cX,cY,pX,pY;var track=function(ev){cX=ev.pageX;cY=ev.pageY};var compare=function(ev,ob){ob.hoverIntent_t=clearTimeout(ob.hoverIntent_t);if((Math.abs(pX-cX)+Math.abs(pY-cY))<cfg.sensitivity){$(ob).unbind("mousemove",track);ob.hoverIntent_s=1;return cfg.over.apply(ob,[ev])}else{pX=cX;pY=cY;ob.hoverIntent_t=setTimeout(function(){compare(ev,ob)},cfg.interval)}};var delay=function(ev,ob){ob.hoverIntent_t=clearTimeout(ob.hoverIntent_t);ob.hoverIntent_s=0;return cfg.out.apply(ob,[ev])};var handleHover=function(e){var ev=jQuery.extend({},e);var ob=this;if(ob.hoverIntent_t){ob.hoverIntent_t=clearTimeout(ob.hoverIntent_t)}if(e.type=="mouseenter"){pX=ev.pageX;pY=ev.pageY;$(ob).bind("mousemove",track);if(ob.hoverIntent_s!=1){ob.hoverIntent_t=setTimeout(function(){compare(ev,ob)},cfg.interval)}}else{$(ob).unbind("mousemove",track);if(ob.hoverIntent_s==1){ob.hoverIntent_t=setTimeout(function(){delay(ev,ob)},cfg.timeout)}}};return this.bind('mouseenter',handleHover).bind('mouseleave',handleHover)}})(jQuery);
+
 $(document).ready(function() {
     initMenu();
 });
 
 
 // Header menu
-
 function initMenu() {
-/*    
-    // Mouse enter
-    $('#headerMenu li').hover(function() {
-        var subMenu = $(this).children('ul');
-        var subMenuHeight = subMenu.show().height();
-        
-        subMenu.hide().css('visibility', 'visible');
-        
-        $('#headerTop').stop().animate({
-           height: 95 + subMenuHeight + 10 // header + bottom padding
-        }, 300);
-        
-        subMenu.stop().delay(150).fadeIn(300);
-    
-    // Mouse leave
-    }, function() {
-         var subMenu = $(this).children('ul');
-         $('#headerTop').height('95px');
-         subMenu.stop().fadeOut(100);
-         
-    });
-    */
    
    function headerOver() {
          var subMenu = $(this).children('ul');
