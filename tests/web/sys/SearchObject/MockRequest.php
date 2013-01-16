3f23790f30d11f6984e9b81e748d669cec7f9f6c
Eero Heikkinen <eero.heikkinen@nba.fi>
2013-01-16 18:46:07 +0200

    Remove old documentation

--- tests/web/sys/SearchObject/MockRequest.php
+++ tests/web/sys/SearchObject/MockRequest.php
@@ -3,38 +3,13 @@
 
 /**
  * Class for mocking response body, code, and supressing sending data.
- * 
- * @uses      Services_TwitPic_Request_HTTPRequest
- * @category  Services
- * @package   Services_TwitPic
- * @author    Bill Shupp <hostmaster@shupp.org> 
- * @copyright 2008 Bill Shupp
- * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
- * @link      http://servicestwitpic.googlecode.com
  */
 class MockRequest extends HTTP_Request
 {
-    /**
-     * Canned response body for testing
-     * 
-     * @var string
-     */
-    private $responseBody;
-
-     /**
-     * Canned response code for testing
-     * 
-     * @var string
-     */
-    private $responseCode;
-    
+    protected $responseBody;
+    protected $responseCode;
     protected $lastQuery;
-
-    /**
-     * Doesn't actually send anything
-     * 
-     * @return void
-     */
+	
     public function sendRequest()
     {
     }
@@ -43,31 +18,16 @@ class MockRequest extends HTTP_Request
         $this->responseBody = $response;
     }
 
-    /**
-     * Returns the canned response code
-     * 
-     * @return void
-     */
     public function getResponseCode()
     {
         return $this->responseCode;
     }
     
-    /**
-     * Returns the canned response code
-     *
-     * @return void
-     */
     public function setResponseCode($code)
     {
         $this->responseCode = $code;
     }
 
-    /**
-     * Returns the canned response body
-     * 
-     * @return void
-     */
     public function getResponseBody()
     {
         return $this->responseBody;
@@ -84,5 +44,4 @@ class MockRequest extends HTTP_Request
     public function getLastQuery() {
         return $this->lastQuery;
     }
-
 }
