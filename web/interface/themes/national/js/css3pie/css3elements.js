70c8bea99da6f74b5131418a6d3c911be223e1b6
tomikkon <tomikkon@mac-kkkl002-tb.lib.helsinki.fi>
2013-01-31 18:10:55 +0200

    National: CSS3PIE modifications for IE.

--- web/interface/themes/national/js/css3pie/css3elements.js
+++ web/interface/themes/national/js/css3pie/css3elements.js
@@ -1,8 +1,146 @@
-/* List of all the CSS3 elements that need PIE to show correcly in IE */
+/* List of all the CSS3 elements that need PIE to show correcly in IE
+   For troubleshooting see: http://css3pie.com/documentation/known-issues/
+*/
+
+/* TODO: Some selectors might be unnecessary, some others still missing.
+         All queries should be combined to one multiple selector query.
+*/
 
 $(document).ready(function() {
     if (window.PIE) {
+/*** border-radius ***/
+        $('.openurlIframe').each(function() {
+          PIE.attach(this);
+        });
+        $('.feedbackButton').each(function() {
+          PIE.attach(this);
+        });
+/** The frame around searchbox from which it is quick to see if PIE works **/
         $('.searchFormWrapper').each(function() {
+          PIE.attach(this);
+        });
+        $('.headerInfoBox').each(function() {
+          PIE.attach(this);
+        });
+        $('.dropdown dd ul, body > ul.ui-menu.ui-widget').each(function() {
+          PIE.attach(this);
+        });
+        $('.dropdown dd ul li:first-child a:hover,
+          body > ul.ui-menu.ui-widget li:first-child a:hover').each(function() {
+          PIE.attach(this);
+        });
+        $('.dropdown dd ul li:last-child a:hover,
+          body > ul.ui-menu.ui-widget li:last-child a:hover').each(function() {
+          PIE.attach(this);
+        });
+        $('body > div.ui-dialog').each(function() {
+          PIE.attach(this);
+        });
+        $('body >.ui-dialog .ui-widget-header .ui-dialog-titlebar-close').each(function() {
+          PIE.attach(this);
+        });
+        $('.searchContextHelp').each(function() {
+          PIE.attach(this);
+        });
+        $('.dropdown dt a').each(function() {
+          PIE.attach(this);
+        });
+        $('.dateVisClear a').each(function() {
+          PIE.attach(this);
+        });
+        $('.dateVisHandle .dateVisHelp .infoIndicator').each(function() {
+          PIE.attach(this);
+        });
+        $('.paginationMove').each(function() {
+          PIE.attach(this);
+        });
+        $('#dyntabnav .ui-tabs-nav').each(function() {
+          PIE.attach(this);
+        });
+        $('.browseNav').each(function() {
+          PIE.attach(this);
+        });
+        $('.sysInfo').each(function() {
+          PIE.attach(this);
+        });
+        $('.button').each(function() {
+          PIE.attach(this);
+        });
+        $('.roundButton').each(function() {
+          PIE.attach(this);
+        });
+        $('.searchFormWrapper').each(function() {
+          PIE.attach(this);
+        });
+/*** box-shadow ***/
+        $('.dateVis').each(function() {
+          PIE.attach(this);
+        });
+        $('.headerInfoBox.visible').each(function() {
+          PIE.attach(this);
+        });
+/*** :before, :after ***/
+        $('.searchContextHelp:before').each(function() {
+          PIE.attach(this);
+        });
+        $('#moreInfoToggle:before, #moreInfoToggle:after').each(function() {
+          PIE.attach(this);
+        });
+        $('.searchContextHelp:after').each(function() {
+          PIE.attach(this);
+        });
+        $('#moreInfoToggle:after').each(function() {
+          PIE.attach(this);
+        });
+        $('#moreInfoToggle.active:after').each(function() {
+          PIE.attach(this);
+        });
+        $('.clearfix:after, .container:after').each(function() {
+          PIE.attach(this);
+        });
+/*** -child ***/
+        $('#headerMenu li a:hover span:first-child,
+          #headerMenu li a:focus span:first-child').each(function() {
+          PIE.attach(this);
+        });
+        $('ul.subMenu li:first-child').each(function() {
+          PIE.attach(this);
+        });
+        $('#header .lang li:first-child').each(function() {
+          PIE.attach(this);
+        });
+        $('.dropdown dd ul li:first-child a').each(function() {
+          PIE.attach(this);
+        });
+        $('.finesList table tr td:first-child').each(function() {
+          PIE.attach(this);
+        });
+        $('.module-Content #main .contentSection:first-child h2:first-child').each(function() {
+          PIE.attach(this);
+        });
+        $('.module-Content #main .contentSection:first-child h2:first-child').each(function() {
+          PIE.attach(this);
+        });
+        $('.searchtools > ul li:first-child').each(function() {
+          PIE.attach(this);
+        });
+        $('.groupSearchHolder .advRow:first-child').each(function() {
+          PIE.attach(this);
+        });
+        $('.groupSearchHolder .advRow.last:first-child').each(function() {
+          PIE.attach(this);
+        });
+        $('.browseNav li:first-child a').each(function() {
+          PIE.attach(this);
+        });
+        $('tr:nth-child(even) td.gridCell').each(function() {
+          PIE.attach(this);
+        });
+        $('tr:nth-child(even) td.gridMouseOver').each(function() {
+          PIE.attach(this);
+        });
+/*** others ***/
+        $('#header .content').each(function() {
           PIE.attach(this);
         });
     }
